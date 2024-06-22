35c474f18d949aede5c36ebc76236054c15f0164
Paulino Auyon <paulinoauyon@outlook.com>
2024-06-21 17:39:43 -0700

    Add new solution for different example, and organize solutions into folders.

---
+++ PubSubThreadChannelDemo/PubSubThreadChannel/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
+var channel = Channel.CreateBounded<string>(new BoundedChannelOptions(100)
+{
+    FullMode = BoundedChannelFullMode.DropOldest
+}, Console.WriteLine);
+
+var writer = channel.Writer;
+var reader = channel.Reader;
+
+var readerTask = Task.Factory.StartNew(async () =>
+{
+    while (!reader.Completion.IsCompleted)
+    {
+        var response = await reader.ReadAsync();
+        Console.WriteLine(response);
+    }
+});
+
+
+for (int i = 0; i < 10; i++)
+{
+    await writer.WriteAsync($"Writer sent message [{i}]");
+    await Task.Delay(1000);
+}
+
+await readerTask;
+Console.WriteLine();
